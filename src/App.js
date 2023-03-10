9f3d3641e747dde226c219b24c0b6b19f967b533
Seruy50 <Seruy50@ukr.net>
2023-03-10 19:17:39 +0200

    tatus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import {VillagesPoints} from './villagespoints'
+import {villages} from './villages'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+  const [currentId, setCurrentId] = useState(1);
+  const [show, setShow] = useState(false)
+
+  
+
+  return <div className="map">
+    <VillagesPoints setShow={setShow} setCurrentId={setCurrentId} show={show} currentId={currentId}/>
+    {show ? <Information villages={villages} id={currentId} show={show} setShow={setShow}/> : ''}
+  </div>
 }
 
-export default App;
+
+
+function Information({villages, id, setShow }){
+  
+  let village = villages.filter(item => item['id'] === id)
+  village = village[0];
+  return <div className='information'>
+    <div className="close" onClick={() => setShow(false)}>
+      <span></span><span></span>
+    </div>
+    <div className="textBlock">
+      <p><b>{village.name}</b></p>
+      {village.transcription}
+      {village.note ? village.note : ''}
+      {village.plus ? village.plus : ''}
+      <ReactAudioPlayer src={village.audio} controls/>
+    </div>
+  </div>
+}
+
